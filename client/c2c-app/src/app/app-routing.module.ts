8aaf4b6c82d4657fd6bf11f4ef42351842a2286c
Melvin Yit <melvinyit@hotmail.com>
2019-12-09 17:16:45 +0800

    routing main link done

--- client/c2c-app/src/app/app-routing.module.ts
+++ client/c2c-app/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/main/homepage/homepage.component';
+import { ListComponent } from './components/main/cars/list.component';
+import { IndividualComponent } from './components/main/cars/individual.component';
+import { FooterComponent } from './components/main/footer/footer.component';
+import { ToolbarComponent } from './components/main/toolbar/toolbar.component';
+import { LoginComponent } from './components/main/authentication/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomepageComponent},
+  {path:'',component:FooterComponent, outlet:'footer'},
+  {path:'',component:ToolbarComponent, outlet:'header'},
+  {path:'home',component:HomepageComponent},
+  {path:'list-cars',component:ListComponent},
+  {path:'list-car/:id1',component:IndividualComponent},
+  {path:'login',component:LoginComponent},
+  {path:'**',redirectTo:'/',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/c2c-app/src/app/app.module.ts
+++ client/c2c-app/src/app/app.module.ts
@@ -12,20 +12,7 @@ import { FAQComponent } from './components/main/other/faq.component';
 import { TnCComponent } from './components/main/other/tn-c.component';
 import { ListComponent } from './components/main/cars/list.component';
 import { IndividualComponent } from './components/main/cars/individual.component';
-import { RegistrationComponent } from './components/renter/registration.component';
-import { BookCarComponent } from './components/renter/book-car/book-car.component';
-import { CheckBookingHistoryComponent } from './components/renter/check-booking-history/check-booking-history.component';
-import { ManageBookingComponent } from './components/renter/manage-booking/manage-booking.component';
-import { AddCarComponent } from './components/owner/add-car/add-car.component';
-import { ManageCarComponent } from './components/owner/manage-car/manage-car.component';
-import { ListBookingComponent } from './components/owner/list-booking/list-booking.component';
-import { ManageProfileComponent } from './components/owner/manage-profile/manage-profile.component';
-import { OTPComponent } from './components/owner/otp/otp.component';
-import { RegisterOTPComponent } from './components/owner/register-otp/register-otp.component';
-import { ManageUsersComponent } from './components/admin/manage-users/manage-users.component';
-import { ManageBookingsComponent } from './components/admin/manage-bookings/manage-bookings.component';
-import { ManageCarsComponent } from './components/admin/manage-cars/manage-cars.component';
-import { SingleComponent } from './components/admin/manageUsers/single.component';
+import { LoginComponent } from './components/main/authentication/login.component';
 
 @NgModule({
   declarations: [
@@ -39,20 +26,7 @@ import { SingleComponent } from './components/admin/manageUsers/single.component
     TnCComponent,
     ListComponent,
     IndividualComponent,
-    RegistrationComponent,
-    BookCarComponent,
-    CheckBookingHistoryComponent,
-    ManageBookingComponent,
-    AddCarComponent,
-    ManageCarComponent,
-    ListBookingComponent,
-    ManageProfileComponent,
-    OTPComponent,
-    RegisterOTPComponent,
-    ManageUsersComponent,
-    ManageBookingsComponent,
-    ManageCarsComponent,
-    SingleComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
