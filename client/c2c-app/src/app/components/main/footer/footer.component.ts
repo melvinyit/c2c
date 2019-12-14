1e3e3e1982d5c087e213cfed701cdcdace008bd8
Melvin Yit <melvinyit@hotmail.com>
2019-12-15 07:32:59 +0800

    mini ui change

--- client/c2c-app/src/app/components/main/footer/footer.component.ts
+++ client/c2c-app/src/app/components/main/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsletterServiceService } from 'src/app/services/newsletter-service.service';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  //constructor() { }
 
   ngOnInit() {
+  }
+
+  constructor(private nlSrv:NewsletterServiceService,private sw:SwPush){}
+  readonly VAPID_PUBLIC_KEY='BDlHHtvlqIbSI_OajHWWM-PdQo6A1dFDR08YcxXgkP2SR6oLr42nDpE-HTqPS_fV-l7OL-L9x2_J7JPEAjtKZ6c';
+	
+  subscribeToNotifications() {
+    console.log(this.VAPID_PUBLIC_KEY);
+      this.sw.requestSubscription({
+          serverPublicKey: this.VAPID_PUBLIC_KEY
+      })
+      .then(sub => {console.log('trying to sub');
+        this.nlSrv.addPushSubscriber(sub).subscribe()})
+      .catch(err => console.error("Could not subscribe to notifications", err));
   }
 
 }
